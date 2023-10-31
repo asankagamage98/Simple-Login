7d9d6d4beabe552431862faacc87579213c9b0cd
asankagamage98 <asankagamage1998@gmail.com>
2023-10-31 18:30:46 +0530

    style login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -37,24 +37,27 @@ export default function Login() {
   return (
     <div className='row '>
         <div className="col-lg-12">  
-            <div className="row">
-               <div className='col-lg-12 layout'>
-                   <form  className='' action="" method="post" onSubmit={(e) =>handleSubmit(e)}>
+            <div className="">
+               <div className='layout'>
+                   <form  className='loginform ' action="" method="post" onSubmit={(e) =>handleSubmit(e)}>
                        <h3 className='mb-3 mt-5'>Login</h3>
 
                        <div className='form-group mb-3'>
                             <label htmlFor="" className='form-lable'> Name</label>
-                            <input type="text" name='name' onChange={(e) => setName(e.target.value)} required autocomplete="off" className='form-control' id="name" />
+                            <input type="text" name='name' onChange={(e) => setName(e.target.value)} required  className='form-control text-box ' id="name" />
                         </div>
                         <div className='form-group mb-3'>
                             <label htmlFor="" className='form-lable'>User email</label>
-                            <input type="email" name='email' onChange={(e) => setEmail(e.target.value)} required autocomplete="off" className='form-control' id="email" />
+                            <input type="email" name='email' onChange={(e) => setEmail(e.target.value)} required  className='form-control text-box ' id="email" />
                         </div>
                         <div className='form-group mb-3'>
                             <label htmlFor="" className='form-lable'>Password</label>
-                            <input type="password" name='password' onChange={(e) => setPassword(e.target.value)} required autocomplete="off"  className='form-control' id="password" />
+                            <input type="password" name='password' onChange={(e) => setPassword(e.target.value)} required  className='form-control text-box ' id="password" />
                         </div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
+                        <div className='d-flex justify-content-start'>
+                            <button type="submit" className="btn btn-info ">Login</button>
+                        </div>
+                       
                  </form>
               </div>
             </div>
